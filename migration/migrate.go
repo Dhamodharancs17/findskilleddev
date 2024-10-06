877455b9efdafed8cd7d93c0b736007937d6e9b3
Dhamodharancs17 <dhamodharan.cs17@dgct.ac.in>
2024-10-06 11:15:17 +0530

    configure migration file

--- migration/migrate.go
+++ migration/migrate.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/Dhamodharancs17/findskilleddev/initializers"
+import (
+	"github.com/Dhamodharancs17/findskilleddev/initializers"
+	"github.com/Dhamodharancs17/findskilleddev/internal/assessment"
+	"github.com/Dhamodharancs17/findskilleddev/internal/candidate"
+	"github.com/Dhamodharancs17/findskilleddev/internal/question"
+	"github.com/Dhamodharancs17/findskilleddev/internal/submission"
+	"github.com/Dhamodharancs17/findskilleddev/internal/user"
+)
 
 func init() {
 	initializers.LoadEnvVariables()
@@ -11,5 +18,10 @@ func main() {
 	//migrate the schema
 	initializers.DB.AutoMigrate(
 		// Add the model files here
+		&assessment.Assessment{},
+		&candidate.Candidate{},
+		&question.Question{},
+		&submission.Submission{},
+		&user.User{},
 	)
 }
